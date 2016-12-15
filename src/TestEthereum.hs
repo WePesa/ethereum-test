27715cd8037638eb9fe13db2546c13fbd62896d3
kejace <kejace@gmail.com>
2016-12-15 16:56:17 +0000

    Added test suite

--- src/TestEthereum.hs
+++ src/TestEthereum.hs
@@ -1,57 +1,61 @@
 {-# LANGUAGE DeriveGeneric, OverloadedStrings, FlexibleInstances, TemplateHaskell #-}
-{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
 module TestEthereum (
-                    runAllTests,
-                    runTests,
-                    runTest,
-                    runTestsInFile
+                      runAllTests
+                    , noLog
                     ) where
 
---import Control.Applicative
+import Control.Applicative
 import Control.Monad
-import Control.Monad.Logger
---import Control.Monad.IfElse
+import Control.Monad.IfElse
 import Control.Monad.IO.Class
+import Control.Monad.Logger
 import Control.Monad.Trans
 import Control.Monad.Trans.Either
---import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Resource
 import Control.Monad.Trans.State
 import qualified Crypto.Hash.SHA3 as SHA3
 import Data.Aeson
 import qualified Data.Binary as Bin
 import qualified Data.ByteString as B
---import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
 import Data.Either
-import qualified Data.Set as Set
 import Data.List
 import qualified Data.Map as M
 import Data.Maybe
+import qualified Data.Set as S
+import HFlags
 import qualified Network.Haskoin.Internals as Haskoin
 import Network.Haskoin.Crypto (withSource)
 import Numeric
-import System.Environment
 import System.Directory
-import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
+import System.Environment
+import System.FilePath
+import Text.PrettyPrint.ANSI.Leijen hiding ((<$>), (</>))
+import qualified Database.LevelDB as DB
 import Blockchain.Output
 
 import Blockchain.BlockChain
 import qualified Blockchain.Colors as C
+import Blockchain.Constants
 import Blockchain.Data.Address
 import Blockchain.Data.AddressStateDB
 import Blockchain.Data.BlockDB
 import Blockchain.Data.Code
 import Blockchain.VMContext
---import Blockchain.Data.DataDefs
+import Blockchain.Data.DataDefs
 import Blockchain.Data.RLP
 import Blockchain.Data.Transaction
-import Blockchain.Data.TransactionDef
-import Blockchain.Database.MerklePatricia
-import Blockchain.DB.CodeDB
+import qualified Blockchain.Database.MerklePatricia as MP
+import Blockchain.Database.MerklePatricia.Internal
+import Blockchain.DB.MemAddressStateDB
 import Blockchain.DB.StateDB
---import Blockchain.DBM
+import Blockchain.DB.StorageDB
+import Blockchain.DB.CodeDB
+import Blockchain.DBM
+--import Blockchain.ExtDBs
 import Blockchain.ExtWord
 import Blockchain.Format
 import Blockchain.SHA
@@ -60,41 +64,35 @@ import Blockchain.VM
 import Blockchain.VM.Code
 import Blockchain.VM.Environment
 import Blockchain.VM.VMState
---import Blockchain.VMOptions
+import Blockchain.VMOptions
+import Blockchain.Sequencer.Event
+import Blockchain.Data.ExecResults
+import qualified Blockchain.Data.TXOrigin as TO
 import qualified Data.NibbleString as N
-import Blockchain.DB.MemAddressStateDB
-import Blockchain.DB.StorageDB
-import Blockchain.Database.MerklePatricia.Internal
-import Blockchain.FastECRecover
---import Blockchain.Data.Address
-import Blockchain.ExtendedECDSA
---import Data.Maybe (fromJust)
-import HFlags 
 
 import TestDescriptions
 
---import Debug.Trace
+import Debug.Trace
 
 import TestFiles
 
---nibbleString2ByteString::N.NibbleString->B.ByteString
---nibbleString2ByteString (N.EvenNibbleString str) = str
---nibbleString2ByteString (N.OddNibbleString c str) = c `B.cons` str
+defineFlag "debugEnabled" False "enable debugging"
+defineFlag "debugEnabled2" False "enable debugging"
 
-emptyStringChar8BS :: Int -> BC.ByteString
-emptyStringChar8BS len = BC.pack $ replicate len '\0'
-
-isDebugEnabled :: Bool
-isDebugEnabled = True
+{-
+nibbleString2ByteString::N.NibbleString->B.ByteString
+nibbleString2ByteString (N.EvenNibbleString str) = str
+nibbleString2ByteString (N.OddNibbleString c str) = c `B.cons` str
+-}
 
 populateAndConvertAddressState::Address->AddressState'->ContextM AddressState
 populateAndConvertAddressState owner addressState' = do
-  addCode . codeBytes . contractCode' $ addressState' -- lift
+  addCode . codeBytes . contractCode' $ addressState'
 
   forM_ (M.toList $ storage' addressState') $
     \(key, val) -> do putStorageKeyVal' owner (fromIntegral key) (fromIntegral val)
 
-  addressState <- getAddressState owner -- lift
+  addressState <- getAddressState owner
 
   return $
     AddressState
@@ -116,17 +114,17 @@ showHexInt x
        ++ xHex
 
 getDataAndRevertAddressState::Address->AddressState->ContextM AddressState'
-getDataAndRevertAddressState _ addressState = do
+getDataAndRevertAddressState owner addressState = do
   theCode <- fmap (fromMaybe (error $ "Missing code in getDataAndRevertAddressState: " ++ format addressState)) $
-             getCode (addressStateCodeHash addressState) -- lift
+             getCode (addressStateCodeHash addressState)
 
   -- Copied wholesale from Context.hs:getAllStorageKeyVals'
   -- since that function requires an unhashed owner.
   -- This piece of code really should be in the lib somewhere
   storage <- do
-    dbs <- get -- lift
+    dbs <- get
     let mpdb = (contextStateDB dbs){stateRoot=addressStateContractRoot addressState}
-    kvs <- unsafeGetKeyVals mpdb "" -- lift $ lift 
+    kvs <- lift $ unsafeGetKeyVals mpdb ""
     return $ map (fmap $ fromInteger . rlpDecode . rlpDeserialize . rlpDecode) kvs
     
   return $
@@ -138,65 +136,81 @@ getDataAndRevertAddressState _ addressState = do
      $ M.fromList storage)
     (Code theCode)
 
---formatAddressState::AddressState'->String
---formatAddressState = show
+formatAddressState::AddressState'->String
+formatAddressState = show
 
 getNumber::String->Integer
 getNumber "" = 0
 getNumber x = read x
 
---newAccountsToCallCreates::(Maybe Address, Integer, AddressState)->ContextM DebugCallCreate
---newAccountsToCallCreates (maybeAddress, gasRemaining, AddressState{balance=b, codeHash=h}) = do
---  Just codeBytes <- lift $ getCode h
---  let destination =
---        case maybeAddress of
---          Just (Address address) -> padZeros 40 $ showHex address ""
---          Nothing -> ""
---  return DebugCallCreate {
---    ccData="0x" ++ BC.unpack (B16.encode codeBytes),
---    ccDestination=destination,
---    ccGasLimit=show gasRemaining,
---    ccValue=show b
---    }
+{-
+newAccountsToCallCreates::(Maybe Address, Integer, AddressState)->ContextM DebugCallCreate
+newAccountsToCallCreates (maybeAddress, gasRemaining, AddressState{balance=b, codeHash=h}) = do
+  Just codeBytes <- lift $ getCode h
+  let destination =
+        case maybeAddress of
+          Just (Address address) -> padZeros 40 $ showHex address ""
+          Nothing -> ""
+  return DebugCallCreate {
+    ccData="0x" ++ BC.unpack (B16.encode codeBytes),
+    ccDestination=destination,
+    ccGasLimit=show gasRemaining,
+    ccValue=show b
+    }
+-}
 
---isBlankCode::Code->Bool
---isBlankCode (Code "") = True
---isBlankCode _ = False
+isBlankCode::Code->Bool
+isBlankCode (Code "") = True
+isBlankCode _ = False
+
+--Just a cheap trick to enable the display of nearly all storage keys in the tests
+someHashes::M.Map SHA Int
+someHashes = M.fromList $ map (\x -> (hash (B.pack $ word256ToBytes x), fromIntegral x)) [0..255]
+
+showHash::Integer->String
+showHash val =
+  case M.lookup (SHA (fromIntegral val)) someHashes of
+   Nothing -> showHexInt val ++ "[#ed]"
+   Just x -> show x
 
 showInfo::(Address,AddressState')->String
-showInfo (key,AddressState'{nonce'=n, balance'=b, storage'=s, contractCode'=Code c}) = 
+showInfo (key,val@AddressState'{nonce'=n, balance'=b, storage'=s, contractCode'=Code c}) = 
     show (pretty key) ++ "[#ed]" ++ "(" ++ show n ++ "): " ++ show b ++ 
          (if M.null s
           then ""
           else ", " ++ (show $ M.toList $
-               M.map showHexInt $ M.mapKeys ((++ "[#ed]") . showHexInt) s)
+               M.map showHexInt $ M.mapKeys showHash s)
          ) ++ 
          (if B.null c then "" else ", CODE:[" ++ C.blue (format c) ++ "]")
-showInfo _ = ""  
 
 addressStates::ContextM [(Address, AddressState')]
 addressStates = do
-  addrStates <- getAllAddressStates -- lift
+  addrStates <- getAllAddressStates
   let addrs = map fst addrStates
       states = map snd addrStates
   states' <- mapM (uncurry getDataAndRevertAddressState) $ zip addrs states
   return $ zip addrs states'
 
---runTest'::Test->ContextM (Either String String)
---runTest' test = do
---    return $ Left ""
+showPart::Show a=>a->String
+showPart x =
+  if length value < 40
+  then value
+  else take 40 value ++ "..."
+  where value = take 40 $ show x
+
+txToOutputTx :: Transaction -> OutputTx
+txToOutputTx = fromJust . wrapTransaction . IngestTx TO.Direct
 
 runTest::Test->ContextM (Either String String)
 runTest test = do
-
-  initializeBlank =<< getStateDB 
+  
+  MP.initializeBlank =<< getStateDB
   setStateDBStateRoot emptyTriePtr
-  --lift $ setStateRoot emptyTriePtr -- TODO add me again
 
   forM_ (M.toList $ pre test) $
     \(addr, s) -> do
       state <- populateAndConvertAddressState addr s
-      putAddressState addr state -- lift
+      putAddressState addr state
 
   beforeAddressStates <- addressStates
 
@@ -207,31 +221,31 @@ runTest test = do
              blockDataNumber = read . currentNumber . env $ test,
              blockDataCoinbase = currentCoinbase . env $ test,
              blockDataDifficulty = read . currentDifficulty . env $ test,
-             blockDataUnclesHash = SHA $ fromIntegral 0, -- error "unclesHash undefined",
-             blockDataStateRoot = StateRoot $ emptyStringChar8BS 32, -- error "bStateRoot undefined",
-             blockDataTransactionsRoot = StateRoot $ emptyStringChar8BS 32, --error "transactionsRoot undefined",
-             blockDataReceiptsRoot = StateRoot $ emptyStringChar8BS 32, --error "receiptsRoot undefined",
-             blockDataLogBloom = emptyStringChar8BS 32, -- error "logBloom undefined",
+             blockDataUnclesHash = SHA 0, --error "unclesHash not set",
+             blockDataStateRoot = StateRoot "", -- error "bStateRoot not set",
+             blockDataTransactionsRoot = StateRoot "", -- error "transactionsRoot not set",
+             blockDataReceiptsRoot = StateRoot "", -- error "receiptsRoot not set", -- StateRoot ""
+             blockDataLogBloom = "", --error "logBloom not set",
              blockDataGasLimit = currentGasLimit . env $ test,
-             blockDataGasUsed = 0, -- error "gasUsed undefined",
+             blockDataGasUsed = 0, --error "gasUsed not set",
              blockDataTimestamp = currentTimestamp . env $ test,
              --timestamp = posixSecondsToUTCTime . fromInteger . read . currentTimestamp . env $ test,
-             blockDataExtraData = 0, -- error "extraData undefined",
-             blockDataNonce = 0, -- error "nonce undefined",
-             blockDataMixHash = SHA $ fromIntegral 0 -- error "mixHash undefined" 
+             blockDataExtraData = 0, --error "extraData not set",
+             blockDataNonce = 0, --error "nonce not set",
+             blockDataMixHash=SHA 0 --error "mixHash not set"
              },
-          blockReceiptTransactions = [], -- error "receiptTransactions undefined",
-          blockBlockUncles = [] -- error "blockUncles undefined"
+          blockReceiptTransactions = [], --error "receiptTransactions not set",
+          blockBlockUncles = [] --error "blockUncles not set"
           }
 
-  (result, retVal, gasRemaining, logs, returnedCallCreates) <-
+  (result, retVal, gasRemaining, logs, returnedCallCreates, maybeVMStateAfter) <-
     case theInput test of
       IExec exec -> do
 
-        let env =
+        let env' =
               Environment{
-                envGasPrice=getNumber $ gasPrice' exec,
-                envBlock=block,
+                envGasPrice = getNumber $ gasPrice' exec,
+                envBlockHeader = blockBlockData $ block,
                 envOwner = address' exec,
                 envOrigin = origin exec,
                 envInputData = theData $ data' exec,
@@ -240,22 +254,30 @@ runTest test = do
                 envCode = code exec,
                 envJumpDests = getValidJUMPDESTs $ code exec
                 }
-        ctx <- get
-        vmState <- liftIO $ startingState False False env ctx -- (error "Context not defined") -- TODO add Context here
 
-        (result, vmState) <-
-          lift $ flip runStateT vmState{vmGasRemaining=getNumber $ gas exec, debugCallCreates=Just []} $ -- - lift
+        cxt <- get
+        vmState <- liftIO $ startingState True False env' cxt
+
+        (result, vmState) <- lift $
+          flip runStateT vmState{vmGasRemaining=getNumber $ gas exec, debugCallCreates=Just []} $
           runEitherT $ do
             runCodeFromStart
 
-            vmState <- lift get 
-            when isDebugEnabled $ do -- liftM
-              liftIO $ putStrLn $ "Removing accounts in suicideList: " ++ 
-                                 intercalate ", " (show . pretty <$> (Set.toList $ suicideList vmState))
+            vmState <- lift get
+            when flags_debugEnabled $ do
+              liftIO $ putStrLn $ "Removing accounts in suicideList: " ++
+                                intercalate ", " (show . pretty <$> S.toList (suicideList vmState))
 
-            forM_ (suicideList vmState) $ deleteAddressState -- lift . lift . lift
+            forM_ (suicideList vmState) $ deleteAddressState
 
-        return (result, returnVal vmState, vmGasRemaining vmState, logs vmState, debugCallCreates vmState)
+        put $ dbs vmState
+
+        flushMemStorageDB
+        flushMemAddressStateDB
+
+        case vmException vmState of
+         Nothing -> return (result, returnVal vmState, vmGasRemaining vmState, logs vmState, debugCallCreates vmState, Just vmState)
+         Just e -> return (Right (), Nothing, 0, [], Just [], Nothing)
 
       ITransaction transaction -> do
         let t = case tTo' transaction of
@@ -276,15 +298,31 @@ runTest test = do
                     (getNumber $ tValue' transaction)
                     (theData $ tData' transaction)
                     (tSecretKey' transaction)
-        signedTransaction <- liftIO $ withSource Haskoin.devURandom t 
+        signedTransaction' <- liftIO $ withSource Haskoin.devURandom t
+        let signedTransaction = txToOutputTx signedTransaction'
         result <-
-          runEitherT $ addTransaction False block (currentGasLimit $ env test) signedTransaction
+          runEitherT $ addTransaction True (blockBlockData $ block) (currentGasLimit $ env test) signedTransaction
+
+        flushMemStorageDB
+        flushMemAddressStateDB
+
+
+-- (result, retVal, gasRemaining, logs, returnedCallCreates, maybeVMStateAfter)
 
         case result of
-          Right (vmState, _) ->
-            return (Right (), returnVal vmState, vmGasRemaining vmState, logs vmState, debugCallCreates vmState)
-          Left e -> return (Right (), Nothing, 0, [], Just [])
+            Right (ExecResults remGas retVal trace logs newCtAddr) -> do
+              return ( Right (), retVal, remGas, logs, Just [], Nothing)
 
+            Left e -> do 
+              return (Right (), Nothing, 0, [], Just [], Nothing)
+
+
+--          Right (VMState{vmException=Just e}, _) -> do
+--                    return (Right (), Nothing, 0, [], Just [], Nothing)
+--          Right (vmState, _) -> do
+--                    return (Right (), returnVal vmState, vmGasRemaining vmState, logs vmState, debugCallCreates vmState, Just vmState)
+--          Left e -> do
+--                    return (Right (), Nothing, 0, [], Just [], Nothing)
 
   afterAddressStates <- addressStates
 
@@ -292,29 +330,29 @@ runTest test = do
       hashAddress (Address s) = Address $ fromIntegral $ byteString2Integer $ nibbleString2ByteString $ N.EvenNibbleString $ (SHA3.hash 256) $ BL.toStrict $ Bin.encode s
 
   let postTest = M.toList $
-                 M.mapKeys hashAddress $
+--                 M.mapKeys hashAddress $
                  flip M.map (post test) $
                  \s' -> s'{storage' = M.mapKeys hashInteger (storage' s')} 
 
-  when isDebugEnabled $ do -- whenM
-    liftIO $ putStrLn "\nBefore   -------------"
+  when flags_debugEnabled $ do
+    liftIO $ putStrLn "Before   -------------"
     liftIO $ putStrLn $ unlines $ showInfo <$> beforeAddressStates
     liftIO $ putStrLn "After    -------------"
     liftIO $ putStrLn $ unlines $ showInfo <$> afterAddressStates
     liftIO $ putStrLn "Expected -------------"
     liftIO $ putStrLn $ unlines $ showInfo <$> postTest
-    liftIO $ putStrLn "End      -------------\n"
+    liftIO $ putStrLn "End      -------------"
 
   case (RawData (fromMaybe B.empty retVal) == out test,
-        (afterAddressStates == postTest) || (null postTest && isLeft result),
+        (M.fromList afterAddressStates == M.fromList postTest) || (null postTest && isLeft result),
         case remainingGas test of
           Nothing -> True
           Just x -> gasRemaining == x,
         logs == reverse (logs' test),
         (callcreates test == fmap reverse returnedCallCreates) || (isNothing (callcreates test) && (returnedCallCreates == Just []))
         ) of
-    (False, _, _, _, _) -> return $ Left "result doesn't match"
-    (_, False, _, _, _) -> return $ Left "address states don't match"
+    (False, _, _, _, _) -> return $ Left $ "result doesn't match" -- : is " ++ showPart retVal ++ ", should be " ++ showPart (out test)
+    (_, False, _, _, _) -> return $ Left $ "address states don't match"
     (_, _, False, _, _) -> return $ Left $ "remaining gas doesn't match: is " ++ show gasRemaining ++ ", should be " ++ show (remainingGas test) ++ ", diff=" ++ show (gasRemaining - fromJust (remainingGas test))
     (_, _, _, False, _) -> do
       liftIO $ putStrLn "llllllllllllllllllllll"
@@ -339,23 +377,16 @@ runTests::[(String, Test)]->ContextM ()
 runTests tests = do
   results <- 
     forM tests $ \(name, test) -> do
-      liftIO $ putStrLn $ "Running test: " ++ show name
+      --liftIO $ putStrLn $ "Running test: " ++ show name
       result <- runTest test
       return (name, result)
   liftIO $ putStrLn $ intercalate "\n" $ formatResult <$> results
 
-whoSignedThisTransaction' :: Transaction -> Maybe Address -- Signatures can be malformed, hence the Maybe
-whoSignedThisTransaction' t = 
-    fmap pubKey2Address $ getPubKeyFromSignature' xSignature theHash
-  where
-    xSignature = ExtendedSignature (Haskoin.Signature (fromInteger $ transactionR t) (fromInteger $ transactionS t)) (0x1c == transactionV t)
-    SHA theHash = partialTransactionHash t
-    getPubKeyFromSignature' = getPubKeyFromSignature_fast
-    partialTransactionHash = hash . rlpSerialize . partialRLPEncode
+noLog _ _ _ _ = do
+  return ()
 
 runAllTests::Maybe String->Maybe String->ContextM ()
 runAllTests maybeFileName maybeTestName= do
-  putWSTT $ fromJust . whoSignedThisTransaction'
   let theFiles =
         case maybeFileName of
           Nothing -> testFiles
